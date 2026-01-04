986ffb307a1821542849392c25a75eff57c8968c
suryadas <suryadassurya603@gmail.com>
2026-01-04 14:47:56 +0530

    Third commit message

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 export default axios.create({
-    baseURL:"https://taskmanagerbackend-1-wm2u.onrender.com"
+    baseURL:"https://taskmanagerbackend-1-wm2u.onrender.com/api/tasks"
 });
